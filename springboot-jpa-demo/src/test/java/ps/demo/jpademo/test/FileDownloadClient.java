8d7849466848d41ad7cdc83800abd8d734f90bdf
yp <yysyp@foxmail.com>
2025-07-22 16:41:19 +0800

    add test and hutool setting

---
+++ springboot-jpa-demo/src/test/java/ps/demo/jpademo/test/FileDownloadClient.java
@@ -0,0 +1,34 @@
+package ps.demo.jpademo.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import ps.demo.commonlibx.common.RestTemplateTool;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Slf4j
+public class FileDownloadClient {
+
+    public static void main(String[] args) throws IOException {
+
+        String url = "xxx";
+        String fileName = "";
+        RestTemplate restTemplate = RestTemplateTool.getRestTemplate();
+        ResponseEntity<byte[]> responseEntity = restTemplate.getForEntity(url, byte[].class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            byte[] fileContent = responseEntity.getBody();
+            if (fileContent != null) {
+                try (FileOutputStream fos = new FileOutputStream(fileName)) {
+                    for (int i = 0; i < fileContent.length; i++) {
+                        fos.write(fileContent[i]);
+                    }
+                }
+            }
+        }
+
+        log.info("Done!");
+
+    }
+}
