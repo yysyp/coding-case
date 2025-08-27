0cd8bb238a8d0d9bb7b50ea775fbd3dbfeb34411
yp <yysyp@foxmail.com>
2025-08-27 19:20:49 +0800

    enhance cmdtool

--- springboot-jpa-demo/src/main/java/ps/demo/commonlibx/common/CmdRunTool2.java
+++ springboot-jpa-demo/src/main/java/ps/demo/commonlibx/common/CmdRunTool2.java
@@ -1,8 +1,10 @@
 package ps.demo.commonlibx.common;
 
+import com.alibaba.excel.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.util.*;
 
 @Slf4j
@@ -13,8 +15,14 @@ public class CmdRunTool2 {
     public final static String EXITCODE = "exitCode";
 
 
-
     public static Map<String, List<String>> runCmds(File directory, Map<String, String> envs, String... commands) {
+        return runCmds(null, directory, envs, commands);
+    }
+    public static Map<String, List<String>> runCmds(String charset, File directory, Map<String, String> envs, String... commands) {
+        //String charset = "GBK";
+        if (StringUtils.isBlank(charset)) {
+            charset = "UTF-8";
+        }
         Map<String, List<String>> outErr = new HashMap<>();
         List<String> out = new ArrayList<>();
         List<String> err = new ArrayList<>();
@@ -39,9 +47,9 @@ public class CmdRunTool2 {
 
             // Read output
             BufferedReader outputReader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
+                    new InputStreamReader(process.getInputStream(), charset));
             BufferedReader errorReader = new BufferedReader(
-                    new InputStreamReader(process.getErrorStream()));
+                    new InputStreamReader(process.getErrorStream(), charset));
 
             String line;
             while ((line = outputReader.readLine()) != null) {
--- springboot-jpa-demo/src/test/java/ps/demo/jpademo/test/GitBashRunnerShFileAndParams2.java
+++ springboot-jpa-demo/src/test/java/ps/demo/jpademo/test/GitBashRunnerShFileAndParams2.java
@@ -7,18 +7,50 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GitBashRunnerShFileAndParams2 {
 
     public static void main(String[] args) throws IOException {
 
-        Map result = CmdRunTool2.runCmds(new File("springboot-jpa-demo/src/main/resources/ignore"),
+        Map<String, List<String>> result = CmdRunTool2.runCmds(new File("springboot-jpa-demo/src/main/resources/ignore"),
                 Maps.of("MY_VAR", "value456").build(),
                 "C:\\Program Files\\Git\\bin\\bash.exe",
                 "-c",
                 "./myscript.sh");
 
-        System.out.println(result);
+        printCmdResult(result);
+
+        Map<String, List<String>> result2 = CmdRunTool2.runCmds("GBK", new File("springboot-jpa-demo/src/main/resources/ignore"),
+                new HashMap<>(),
+                "cmd.exe",
+                "/c",
+                "ipconfig /all");
+
+        printCmdResult(result2);
+
+        //Linux processBuilder.command("ls", "-al");
+        Map<String, List<String>> result3 = CmdRunTool2.runCmds("GBK", new File("springboot-jpa-demo/src/main/resources/ignore"),
+                new HashMap<>(),
+                "C:\\Program Files\\Git\\bin\\bash.exe",
+                "ls",
+                "-al");
+
+        printCmdResult(result3);
+
+    }
+
+    public static void printCmdResult(Map<String, List<String>> result) {
+        System.out.println("ExitCode: "+result.get(CmdRunTool2.EXITCODE));
+        List<String> out = result.get(CmdRunTool2.OUT);
+        for (int i = 0, n = out.size(); i < n; i++) {
+            System.out.println("<"+i+">: " + out.get(i));
+        }
+        List<String> err = result.get(CmdRunTool2.ERR);
+        for (int i = 0, n = err.size(); i < n; i++) {
+            System.out.println("<"+i+">: " + err.get(i));
+        }
     }
 }
