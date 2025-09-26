3be3d931f2405a337b43fa89c55a67ee8089a814
yp <yysyp@foxmail.com>
2025-09-26 21:06:18 +0800

    fix

--- springboot-jpa-demo/src/main/java/ps/demo/jpademo/config/AuditAspect.java
+++ springboot-jpa-demo/src/main/java/ps/demo/jpademo/config/AuditAspect.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import ps.demo.jpademo.entity.OperationAuditLog;
 import ps.demo.jpademo.repository.OperationAuditLogRepository;
@@ -32,7 +34,12 @@ public class AuditAspect {
     @AfterReturning(pointcut = "controllerMethods()", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
         OperationAuditLog operationAuditLog = new OperationAuditLog();
-        operationAuditLog.setUserId("anonymous"); // Replace with actual user ID from security context
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userId = authentication != null ? authentication.getName() : "anonymous";
+        operationAuditLog.setUserId(userId);
+
+        //operationAuditLog.setUserId("anonymous"); // Replace with actual user ID from security context
         operationAuditLog.setAction(joinPoint.getSignature().getName());
         operationAuditLog.setMethodName(joinPoint.getSignature().toShortString());
         //endpointAuditLog.setParameters(joinPoint.getArgs().toString());
