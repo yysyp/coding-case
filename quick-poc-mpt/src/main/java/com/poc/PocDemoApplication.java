c750c13e0d00ad2b0b0cc801c457bafb44ac6db0
yp <yysyp@foxmail.com>
2025-12-10 10:37:42 +0800

    quick poc mpt

---
+++ quick-poc-mpt/src/main/java/com/poc/PocDemoApplication.java
@@ -0,0 +1,12 @@
+package com.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocDemoApplication.class, args);
+    }
+}
