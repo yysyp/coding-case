cee553bd7721ecac6ce09b2d9ef30cbe9ef7edb9
yp <yysyp@foxmail.com>
2025-12-11 12:07:05 +0800

    aaa

--- quick-poc-redis/doc/docker-redis.sh
+++ quick-poc-redis/doc/docker-redis.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-#docker pull redis:6.2.6
+#export USER_NAME=`cat ./ignore/user.txt`
+#export PASS_WORD=`cat ./ignore/pass.txt`
+#echo "$PASS_WORD" | docker login nexus.xxx:0000 --username "$USER_NAME" --password-stdin
+#docker pull nexus.xxx:0000/redis:6.2.6
+#docker tag nexus.xxx:0000/redis:6.2.6 redis:6.2.6
+#docker run --restart=always -p 6379:6379 redis:6.2.6 redis-server
+
 docker pull redis
 docker run --restart=always -p 6379:6379 --name my-redis -d redis redis-server
-
 
