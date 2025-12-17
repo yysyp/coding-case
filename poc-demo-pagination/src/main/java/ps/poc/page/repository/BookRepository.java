1e90254cb0108f26c5ba5bcccd0793ca62718416
yp <yysyp@foxmail.com>
2025-12-17 21:40:31 +0800

    add native query

--- poc-demo-pagination/src/main/java/ps/poc/page/repository/BookRepository.java
+++ poc-demo-pagination/src/main/java/ps/poc/page/repository/BookRepository.java
@@ -27,4 +27,29 @@ public interface BookRepository extends JpaRepository<Book, Long> {
         @Param("author") String author, 
         Pageable pageable
     );
+
+//Equivalent to the following native query
+//    @Query(value = "SELECT * FROM books WHERE LOWER(title) LIKE LOWER(CONCAT('%', :title, '%'))",
+//            countQuery = "SELECT COUNT(*) FROM books WHERE LOWER(title) LIKE LOWER(CONCAT('%', :title, '%'))",
+//            nativeQuery = true)
+//    Page<Book> findByTitleContainingIgnoreCase(@Param("title") String title, Pageable pageable);
+//
+//
+//    @Query(value = "SELECT * FROM books WHERE LOWER(author) LIKE LOWER(CONCAT('%', :author, '%'))",
+//            countQuery = "SELECT COUNT(*) FROM books WHERE LOWER(author) LIKE LOWER(CONCAT('%', :author, '%'))",
+//            nativeQuery = true)
+//    Page<Book> findByAuthorContainingIgnoreCase(@Param("author") String author, Pageable pageable);
+//
+//    @Query(value = "SELECT * FROM books b WHERE " +
+//            "(:title IS NULL OR LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))) AND " +
+//            "(:author IS NULL OR LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%')))",
+//            countQuery = "SELECT COUNT(*) FROM books b WHERE " +
+//                    "(:title IS NULL OR LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))) AND " +
+//                    "(:author IS NULL OR LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%')))",
+//            nativeQuery = true)
+//    Page<Book> findByTitleAndAuthorContainingIgnoreCase(
+//            @Param("title") String title,
+//            @Param("author") String author,
+//            Pageable pageable
+//    );
 }
